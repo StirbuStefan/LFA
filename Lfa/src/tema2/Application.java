e60f96e377f63f467e0b34659529931e3d1c5a1a
StirbuStefan <93604440+StirbuStefan@users.noreply.github.com>
2023-05-17 00:27:39 +0300

    Add files via upload

---
+++ Lfa/src/tema2/Application.java
@@ -0,0 +1,15 @@
+package tema2;
+
+import javax.swing.JFrame;
+
+public class Application {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Graph Drawer");		
+		Swing app = new Swing();
+		frame.add(app);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
